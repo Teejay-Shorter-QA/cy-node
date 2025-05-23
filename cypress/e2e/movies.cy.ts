aaabae57c2527601a846a0d6129d5702003b0bbf
Teejay Shorter <teejayshorter@hotmail.com>
2025-05-23 10:12:20 +0100

    Middleware Part 4

--- cypress/e2e/movies.cy.ts
+++ cypress/e2e/movies.cy.ts
@@ -1,3 +1,4 @@
+import 'cypress-ajv-schema-validator';
 import { retryableBefore } from 'cy-retryable-before';
 import spok from 'cy-spok';
 import schema from '../../src/api-docs/openapi.json';
@@ -24,7 +25,145 @@ describe('CRUD movie', () => {
     });
   });
 
-  it('should', () => {
-    cy.createMovie(tokenMessage, movie);
+  it('should create a movie', () => {
+    cy.createMovie(tokenMessage, movie)
+      .validateSchema(typedSchema, {
+        endpoint: '/movies',
+        method: 'POST'
+      })
+      .its('body')
+      .should(
+        spok({
+          status: 200,
+          data: movieProps
+        })
+      );
+  });
+
+  it('should get all movies', () => {
+    cy.getMovies(tokenMessage)
+      .validateSchema(typedSchema, {
+        endpoint: '/movies',
+        method: 'GET'
+      })
+      .its('body')
+      .should(
+        spok({
+          status: 200,
+          data: (moviesArray: Movie[]) =>
+            moviesArray.map(
+              spok({
+                id: spok.number,
+                ...movieProps
+              })
+            )
+        })
+      );
+  });
+
+  it('should get movie by id', () => {
+    // Create a new movie and save its id
+    cy.createMovie(tokenMessage, generateMovieWithoutId())
+      .its('body.data.id')
+      .then((id: number) => {
+        // Get the newly creted movie by id
+        cy.getMovieById(tokenMessage, id)
+          .validateSchema(typedSchema, {
+            endpoint: '/movies/{id}',
+            method: 'GET'
+          })
+          .its('body')
+          .should(
+            spok({
+              status: 200,
+              data: movieProps
+            })
+          );
+      });
+  });
+
+  it('should get movie by name', () => {
+    // Create a new movie and save its id
+    cy.createMovie(tokenMessage, generateMovieWithoutId())
+      .its('body.data.name')
+      .then((name: string) => {
+        // Get the newly creted movie by id
+        cy.getMovieByName(tokenMessage, name)
+          .validateSchema(typedSchema, {
+            endpoint: '/movies',
+            method: 'GET'
+          })
+          .its('body')
+          .should(
+            spok({
+              status: 200,
+              data: movieProps
+            })
+          );
+      });
+  });
+
+  it('should update a movie', () => {
+    // Create a new movie and save its id
+    cy.createMovie(tokenMessage, generateMovieWithoutId())
+      .its('body.data.id')
+      .then((id: number) => {
+        // Get the newly creted movie by id
+        cy.updateMovie(tokenMessage, id, updatedMovie)
+          .validateSchema(typedSchema, {
+            endpoint: '/movies/{id}',
+            method: 'PUT',
+            status: 200
+          })
+          .its('body')
+          .should(
+            spok({
+              status: 200,
+              data: { ...movieProps, id }
+            })
+          );
+      });
+  });
+
+  it('should delete a movie', () => {
+    // Create a new movie and save its id
+    const movieData = generateMovieWithoutId();
+    cy.createMovie(tokenMessage, movieData)
+      .its('body.data.id')
+      .then((id: number) => {
+        // Get the newly creted movie by id
+        cy.deleteMovie(tokenMessage, id)
+          .validateSchema(typedSchema, {
+            endpoint: '/movies/{id}',
+            method: 'DELETE',
+            status: 200
+          })
+          .its('body')
+          .should(
+            spok({
+              status: 200,
+              message: `Movie ID: ${id} deleted successfully`
+            })
+          );
+      });
+    cy.getMovies(tokenMessage)
+      .findOne({ name: movieData.name })
+      .should('not.exist');
+  });
+
+  it('should handle deleting a non-existant movie', () => {
+    cy.deleteMovie(tokenMessage, 999, true)
+      .validateSchema(typedSchema, {
+        endpoint: '/movies/{id}',
+        method: 'DELETE',
+        status: 404
+      })
+      .its('body')
+      .should(
+        spok({
+          status: 404,
+          error: 'Movie ID: 999 not found'
+        })
+      );
   });
 });
